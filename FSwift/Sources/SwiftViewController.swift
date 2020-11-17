0bcc50c79759229baa762e803e89e7c75f0c8c98
liangc <liangc@yxt.com>
2020-11-17 16:47:23 +0800

    添加文件

---
+++ FSwift/Sources/SwiftViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SwiftViewController.swift
+//  FSwift
+//
+//  Created by UWLiang on 2020/11/17.
+//
+
+import UIKit
+
+class SwiftViewController: UIViewController {
+    
+    lazy var helloLabel: UILabel = {
+        let label = UILabel()
+        label.text = "这是一个Swift写的视图"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+
+        view.addSubview(helloLabel)
+        helloLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        helloLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+
+}
---
+++ Framework/FSwift.framework/Headers/FSwift.h
@@ -0,0 +1,18 @@
+//
+//  FSwift.h
+//  FSwift
+//
+//  Created by UWLiang on 2020/11/17.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FSwift.
+FOUNDATION_EXPORT double FSwiftVersionNumber;
+
+//! Project version string for FSwift.
+FOUNDATION_EXPORT const unsigned char FSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FSwift/PublicHeader.h>
+
+
